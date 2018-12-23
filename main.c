9176c37f9cf832754eff407ab4d6291fce625de9
Ubuntu <ubuntu@ip-172-31-21-184.us-east-2.compute.internal>
2018-12-23 00:18:26 -0800

    pt.2, left off after creating both enums.

--- main.c
+++ main.c
@@ -53,6 +53,26 @@ void read_input(InputBuffer* input_buffer){
     input_buffer->buffer[bytes_read - 1] = 0;
 }
 /*
+*name: MetaCommandResult_t
+*type: enumerator 
+*desc:
+*/
+enum MetaCommandResult_t{
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+};
+typedef enum MetaCommandResult_t MetaCommandResult;
+/*
+*name: PreparedResult_t
+*type: enumerator 
+*desc:
+*/
+enum PreparedResult_t {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+};
+typedef enum PreparedResult_t PreparedResult
+/*
 *Main function has an infinite loop that prints the 
 *prompt, gets a line of input, then processes that 
 *line of input.
@@ -64,13 +84,45 @@ int main(int argc, char* argv[]){
     while(true){
         print_prompt();
         read_input(input_buffer);
-        
+        /*
+        *
+        * PART 1 CODE HERE
         if(strcmp(input_buffer->buffer, ".exit")==0){
             exit(EXIT_SUCCESS);
         }
         else{
             printf("Unrecognized command '%s' .\n", input_buffer->buffer);
         }
-    }
+        * END PART ONE CODE
+        */ 
+        /*
+        *
+        * SQLite meta commands that start with dot operator '.'
+        */
+        if (input_buffer->buffer[0]) == '.'{
+            switch (do_meta_command(input_buffer)){
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                    continue;
+            }//end switch
+        }//end if
+        Statement statement;
+        /*
+        * SQLite "frontend"
+        * line of input into our internal representation of a statement.
+        */
+        switch (prepare_statement(input_buffer, &statement)){
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start '%s'.\n", input_buffer->buffer);
+                continue;
+        }//end switch
+        //execute_command() is our virtual machine.
+        execute_statement(&statement);
+        printf("Executed.\n");
+    }//end while loop
     return 0;   
 }
